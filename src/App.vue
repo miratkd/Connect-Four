d15db16dca0b1c7ddaecf00544d2b477a9eab31b
Mira <lucas-mira1@hotmail.com>
2023-07-10 15:02:32 -0300

    0.0.1 H1 and p

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <router-view/>
+    
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+</style>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+<style>
+body{
+  margin: 0;
+  font-size: 1vw;
+  font-family: Space Grotesk;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+h1{
+  text-shadow: 3px 3px 4px gray;
+  font-size: 3.9em;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.5;
+  margin: 0;
+}
+h2{
+  font-size: 1.7em;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.5;
+  margin: 0;
+}
+h3{
+  font-size: 1.4em;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.5;
+  margin: 0;
+}
+h4{
+  font-size: 1.2em;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.5;
+  margin: 0;
+}
+p{
+  font-family: Space Grotesk;
+  font-size: 1.2em;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  opacity: 0.6;
+  margin: 0;
 }
 </style>
